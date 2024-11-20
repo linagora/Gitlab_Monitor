5fc973c9eb27e31ce2c43f0e3e3ef9dee5c1b8dd
Maïlys Jara <mjara@linagora.com>
2024-11-20 17:55:14 +0100

    build: ce sont des commits de debug pour l'implémentation de la pipeline ;)

--- scripts/generate_provenances.sh
+++ scripts/generate_provenances.sh
@@ -3,11 +3,8 @@ set -e
 
 mkdir -p build
 
-tar -czf build/gitlab-monitor.tar ./gitlab-monitor
-
 cat <<EOF > build/provenance.json
 {
-  "artifact": "build/gitlab-monitor.tar",
   "commit": "$CI_COMMIT_SHA",
   "pipeline": "$CI_PIPELINE_ID",
 }
--- scripts/verify_provenances.sh
+++ scripts/verify_provenances.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 set -e
 
-ARTIFACT_FILE="build/gitlab-monitor.tar"
 PROVENANCE_FILE="build/provenance.json"
-
-if [ ! -f "$ARTIFACT_FILE" ]; then
-  echo "Error: Artifact file $ARTIFACT_FILE not found."
-  exit 1
-fi
 
 if [ ! -f "$PROVENANCE_FILE" ]; then
   echo "Error: Provenance file $PROVENANCE_FILE not found."
